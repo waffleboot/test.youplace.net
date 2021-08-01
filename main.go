761fb280883bb81235d109ef24aa19e01d8c7273
Andrei Yangabishev <waffleboot@gmail.com>
2021-08-01 16:42:55 +0300

    переименование

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ type cli struct {
 	url    string
 }
 
-func (c *cli) checkRedirect(req *http.Request, _ []*http.Request) error {
+func (c *cli) saveRedirect(req *http.Request, _ []*http.Request) error {
 	c.url = req.URL.String()
 	return nil
 }
@@ -33,7 +33,7 @@ func newCli() (*cli, error) {
 	c := &cli{client: &http.Client{
 		Jar: jar,
 	}}
-	c.client.CheckRedirect = c.checkRedirect
+	c.client.CheckRedirect = c.saveRedirect
 	return c, nil
 }
 
@@ -51,26 +51,6 @@ func (c *cli) get(url string) (string, error) {
 	return b.String(), err
 }
 
-func (c *cli) req1() error {
-	body, err := c.get(mainUrl)
-	link, err := findQuestion1Link(body)
-	if err != nil {
-		return err
-	}
-	c.url = mainUrl + link
-	return nil
-}
-
-func (c *cli) req2() error {
-	body, err := c.get(c.url)
-	if err != nil {
-		return err
-	}
-	for data := parseString(body); err == nil && data != nil; data, err = c.post(data) {
-	}
-	return err
-}
-
 func (c *cli) post(data url.Values) (url.Values, error) {
 	resp, err := c.client.PostForm(c.url, data)
 	if err != nil {
@@ -83,21 +63,38 @@ func (c *cli) post(data url.Values) (url.Values, error) {
 	return parseReader(resp.Body)
 }
 
-func (c *cli) run() (err error) {
-	for _, m := range []func() error{c.req1, c.req2} {
-		if err = m(); err != nil {
-			return
-		}
+func (c *cli) parseInitialPage() error {
+	body, err := c.get(mainUrl)
+	if err != nil {
+		return err
 	}
-	return
+	link, err := findQuestion1Link(body)
+	if err != nil {
+		return err
+	}
+	c.url = mainUrl + link
+	return nil
+}
+
+func (c *cli) parseNextPages() error {
+	body, err := c.get(c.url)
+	if err != nil {
+		return err
+	}
+	for data := parseString(body); err == nil && data != nil; data, err = c.post(data) {
+	}
+	return err
 }
 
 func main() {
-	c, err := newCli()
+	cli, err := newCli()
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err := c.run(); err != nil {
+	if err := cli.parseInitialPage(); err != nil {
+		log.Fatal(err)
+	}
+	if err := cli.parseNextPages(); err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("done")
